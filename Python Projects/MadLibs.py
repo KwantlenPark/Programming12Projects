70731c9438de2483664e02ef31dc30371f873f20
KwantlenPark <71847572+KwantlenPark@users.noreply.github.com>
2022-03-28 12:12:41 -0700

    Add files via upload

---
+++ Python Projects/MadLibs.py
@@ -0,0 +1,6 @@
+noun = input("enter a noun: ")
+verb1 = input("enter something else for verb:"  )
+
+madlib = "One day " + noun + "was playing. When " + noun + " " + verb1 + " reall fast!."
+
+print(madlib)
